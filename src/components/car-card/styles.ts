8444bd565c0385d511cc5b743e0141550eee595a
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-11 18:31:59 -0300

    refactor(components/car-card): change styles

--- src/components/car-card/styles.ts
+++ src/components/car-card/styles.ts
@@ -2,8 +2,6 @@ import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.article`
   ${({ theme }) => css`
-    width: 28.7rem;
-    height: 22.3rem;
     background-color: ${theme.colors.shape};
     border-radius: 20px;
     padding: 2.3rem 1.7rem;
@@ -27,10 +25,10 @@ export const Brand = styled.h3`
 export const Model = styled.p`
   font-weight: 100;
   text-transform: uppercase;
-  margin-bottom: 0.6rem;
 `
 
 export const ThumbWrapper = styled.div`
+  padding-top: 1.8rem;
   height: 107px;
   display: flex;
   justify-content: center;
@@ -41,10 +39,10 @@ export const Thumb = styled.img`
   width: 100%;
   display: block;
   object-fit: cover;
-  margin-bottom: 0.3rem;
 `
 
 export const Rent = styled.p`
+  padding-top: 0.6rem;
   align-self: flex-end;
   font-size: 2.4rem;
   font-weight: bold;

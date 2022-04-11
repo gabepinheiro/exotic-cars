3e16c9e1b906fa3a265a4a075bbc02012f328605
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-11 19:04:50 -0300

    refactor(pages/home): change styles

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   padding: 2.2rem 0;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(28.7rem, max-content));
-  grid-gap: 2.2rem 3.2rem;
+  grid-template-columns: repeat(auto-fill, minmax(28.7rem, 1fr));
+  grid-gap: 3.2rem 2.4rem;
 `

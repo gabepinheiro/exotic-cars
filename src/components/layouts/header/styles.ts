d74cfe352f438467e944b9ff87ed578e5c886a5e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-11 18:20:24 -0300

    feat(layouts/header): adding position relative and z-index

--- src/components/layouts/header/styles.ts
+++ src/components/layouts/header/styles.ts
@@ -6,6 +6,8 @@ export const Wrapper = styled.header`
     background-color: ${theme.colors.background};
     height: 8.5rem;
     box-shadow: 0px 10px 30px #0000001A;
+    position: relative;
+    z-index: 1;
 
     ${ContentContainer} {
       display: flex;

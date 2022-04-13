bd15f964f410fc8dc034a4e96428930790b2bac4
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-13 11:43:15 -0300

    refactor(data/car): updating and adding remaining data on all objects

--- src/resources/data/cars.ts
+++ src/resources/data/cars.ts
@@ -9,17 +9,17 @@ export const cars = [
     colors: [
       {
         id: 1,
-        color: 'red',
+        color: 'Red',
         image: 'assets/ferrari/italia/red-color.png',
       },
       {
         id: 2,
-        color: 'gray',
+        color: 'Gray',
         image: 'assets/ferrari/italia/gray-color.png',
       },
       {
         id: 3,
-        color: 'yellow',
+        color: 'Yellow',
         image: 'assets/ferrari/italia/yellow-color.png',
       },
     ],
@@ -27,106 +27,376 @@ export const cars = [
   {
     brand: 'Nissan',
     model: 'GT-R',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/nissan/gtr/thumb.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Mclaren',
     model: '570s',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/mclaren-570s.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'BMW',
     model: 'M6 Gran Coupe',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/bmw-m6-gran-coupe.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Porsche',
     model: '911 GT3 RS',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/porsche-911-gt3-rs.png',
     rentAmount: 825,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Bentley',
     model: 'Continental GT',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/bentley-continental-gt.png',
     rentAmount: 825,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'BMW',
     model: 'M1 Competition',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/bmw-m1-competition.png',
     rentAmount: 825,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Alfa Romeo',
     model: '4c Spider',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/alfa-romeo-4c-spider.png',
     rentAmount: 825,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'McLaren',
     model: 'P1 GTR',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/mclaren-p1-gtr.png',
     rentAmount: 925,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Lamborghini',
     model: 'Aventador',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/lamborghini-aventador.png',
     rentAmount: 925,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Bugatti',
     model: 'Aventador',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/bugatti-veryon-spyder.png',
     rentAmount: 925,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Chevrolet',
     model: 'Corvett',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/chevrolet-corvett.png',
     rentAmount: 925,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Ferrari',
     model: 'Italia',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/ferrari/italia/thumb.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Rolls Royce',
     model: 'Wraith',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/rolls-royce-wraith.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Porsche',
     model: '911 GT3',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/porsche-911-gt3.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
   {
     brand: 'Mercedes-benz',
     model: 'AMG GTS',
+    brandImage: 'assets/nissan/brand.png',
     thumb: 'assets/thumbs/mercedes-amg-gts.png',
     rentAmount: 725,
     per: 'day',
+    colors: [
+      {
+        id: 1,
+        color: 'Black',
+        image: 'assets/nissan/gtr/black-color.png',
+      },
+      {
+        id: 2,
+        color: 'Gray',
+        image: 'assets/nissan/gtr/gray-color.png',
+      },
+      {
+        id: 3,
+        color: 'Orange',
+        image: 'assets/nissan/gtr/orange-color.png',
+      },
+    ],
   },
 ]

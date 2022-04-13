fd8632a66f51cf5adef6509495d5e57f3180f861
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-13 10:43:03 -0300

    feat(pages/home): adding Link

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Car } from '@/resources/types'
 import { CarCard } from '@/components/car-card'
 import { http } from '@/service'
@@ -24,7 +25,9 @@ function HomePage () {
   return (
     <S.Wrapper>
       {cars.map(({ id, ...props }) => (
-        <CarCard key={id} {...props} />
+        <Link key={id} to={`/car-details/${id}`}>
+          <CarCard key={id} {...props} />
+        </Link>
       ))}
     </S.Wrapper>
   )
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -5,4 +5,9 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(28.7rem, 1fr));
   grid-gap: 3.2rem 2.4rem;
+
+  a {
+    text-decoration: none;
+    color: currentColor;
+  }
 `

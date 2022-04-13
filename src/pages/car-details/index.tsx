691ad6723a59009ace012e3721001cf94254eea2
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-04-13 11:30:53 -0300

    feat(car-details): retrieving params from the route and search the car by id

--- src/pages/car-details/index.tsx
+++ src/pages/car-details/index.tsx
@@ -1,25 +1,59 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Car } from '@/resources'
+import { http } from '@/service'
 import { ButtonLink, CarSlider } from '@/components'
 
 import * as S from './styles'
 
 function CarDetailsPage () {
+  const { carId } = useParams<{carId: string}>()
+  const [isFetching, setIsFetching] = useState(true)
+  const [car, setCar] = useState<Car | null>(null)
+  const [carColorIndex, setCarColorIndex] = useState<number>(0)
+
+  useEffect(() => {
+    const fetchCar = async () => {
+      try {
+        const { data: { car: carData } } = await http.get(`/cars/${carId}`)
+        setCar(carData)
+      } catch (error) {
+
+      } finally {
+        setIsFetching(false)
+      }
+    }
+
+    fetchCar()
+  }, [carId])
+
+  if (isFetching && !car) {
+    return null
+  }
+
   return (
     <S.Wrapper>
       <S.CarInfo>
-        <S.BrandImage src='assets/ferrari/brand.png' alt='Ferrari' />
+        <S.BrandImage
+          src={car?.brandImage}
+          alt={`${car?.brand} ${car?.model}`}
+        />
         <S.BrandModelRentWrapper>
-          <S.BrandModel>Ferrari Italia</S.BrandModel>
-          <S.Rent>$725/day</S.Rent>
+          <S.BrandModel>{car?.brand} {car?.model}</S.BrandModel>
+          <S.Rent>{car?.rentAmount}/{car?.per}</S.Rent>
         </S.BrandModelRentWrapper>
       </S.CarInfo>
       <S.CarImageWrapper>
         <ButtonLink to='#' reverse='true' variant='outlined'>
           Back to catalog
         </ButtonLink>
-        <S.CarImage src='assets/ferrari/italia/gray-color.png' />
+        <S.CarImage
+          src={car?.colors[carColorIndex].image}
+          alt={car?.model}
+        />
         <S.Color>
-          <span>03</span>
-          <span className='colorName'>Gray</span>
+          <span>{String(carColorIndex + 1).padStart(2, '0')}</span>
+          <span className='colorName'>{car?.colors[carColorIndex].color}</span>
         </S.Color>
       </S.CarImageWrapper>
       <S.Center>
